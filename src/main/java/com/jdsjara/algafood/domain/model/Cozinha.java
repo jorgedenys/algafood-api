f52e897a0df45898991ee6aa5f02c5fcaae716b6
Jorge Denys <jorgedenys@gmail.com>
2023-10-06 16:38:53 -0300

    Cozinha oneToMany Restaurante

--- src/main/java/com/jdsjara/algafood/domain/model/Cozinha.java
+++ src/main/java/com/jdsjara/algafood/domain/model/Cozinha.java
@@ -3,6 +3,7 @@ package com.jdsjara.algafood.domain.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonRootName;
 
 import jakarta.persistence.Column;
@@ -28,6 +29,9 @@ public class Cozinha {
 	@Column(nullable = false)
 	private String nome;
 	
+	// Mapeamento feito apenas para aprendizado. Esse mapeamento só deverá ser feito
+	// se caso realmente precise da informação dos restaurantes por cozinha.
+	@JsonIgnore
 	@OneToMany(mappedBy = "cozinha")
 	private List<Restaurante> restaurantes = new ArrayList<>();
 	
