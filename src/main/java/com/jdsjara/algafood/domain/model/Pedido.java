a4b96e10eec4901e007a5060e4e3cfe383318b8d
Jorge Denys <jorgedenys@gmail.com>
2023-10-14 14:33:23 -0300

    Criando as tabelas e mapeamento Pedido e Item Pedido

---
+++ src/main/java/com/jdsjara/algafood/domain/model/Pedido.java
@@ -0,0 +1,71 @@
+package com.jdsjara.algafood.domain.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.jdsjara.algafood.domain.enums.StatusPedido;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Pedido {
+	
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private BigDecimal subtotal;
+	
+	@Column(nullable = false)
+	private BigDecimal taxaFrete;
+	
+	@Column(nullable = false)
+	private BigDecimal valorTotal;
+	
+	@CreationTimestamp
+	@Column(nullable = false)
+	private LocalDateTime dataCriacao;
+
+	private LocalDateTime dataConfirmacao;
+	private LocalDateTime dataCancelamento;
+	private LocalDateTime dataEntrega;
+	
+	private StatusPedido status;
+	
+	@Embedded
+	private Endereco enderecoEntrega;
+	
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private FormaPagamento formaPagamento;
+	
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private Restaurante restaurante;
+	
+	@ManyToOne
+	@JoinColumn(name = "usuario_cliente_id", nullable = false)
+	private Usuario cliente;
+	
+	@OneToMany(mappedBy = "pedido")
+	private List<ItemPedido> itens = new ArrayList<>();
+	
+}
