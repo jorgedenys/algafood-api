6d1d775382f53813e953fdb457c163d21ee1031d
Jorge Denys <jorgedenys@gmail.com>
2023-10-22 17:01:55 -0300

    Criando uma consulta findAll() em RestauranteRepository com left join fetch

--- src/main/java/com/jdsjara/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/jdsjara/algafood/domain/repository/RestauranteRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.jdsjara.algafood.domain.model.Restaurante;
@@ -13,7 +14,14 @@ import com.jdsjara.algafood.domain.model.Restaurante;
 public interface RestauranteRepository 
 		extends CustomJpaRepository<Restaurante, Long>, RestauranteRepositoryQueries,
 		JpaSpecificationExecutor<Restaurante> {
-
+	
+	// Se um restaurante não tiver nenhuma forma de pagamento associada a ele,
+    // esse restaurante não será retornado usando JOIN FETCH r.formasPagamento.
+	// Para resolver isso, temos que usar LEFT JOIN FETCH r.formasPagamento
+    // @Query("from Restaurante r join fetch r.cozinha join fetch r.formasPagamento")
+	@Query("from Restaurante r join fetch r.cozinha left join fetch r.formasPagamento")
+	List<Restaurante> findAll();
+	
 	List<Restaurante> queryByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
 	
 	Optional<Restaurante> findFirstRestauranteByNomeContaining(String nome);
