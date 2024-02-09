4c72939a5be70d46c014e909def6140fb8f35edb
Jorge Denys <jorgedenys@gmail.com>
2024-02-09 00:59:29 -0300

    Implementando upload de arquivo com multipart/form-data

---
+++ src/main/java/com/jdsjara/algafood/api/model/input/FotoProdutoInput.java
@@ -0,0 +1,16 @@
+package com.jdsjara.algafood.api.model.input;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FotoProdutoInput {
+	
+	private MultipartFile arquivo;
+	
+	private String descricao;
+	
+}
