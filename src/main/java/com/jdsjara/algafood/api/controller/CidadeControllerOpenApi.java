e06f18a7aba9dfa8df6eda799e87c1fa899a4b84
Jorge Denys <jorgedenys@gmail.com>
2024-02-27 11:40:59 -0300

    Integrando o serviço de catálogo de fotos com o serviço de armazenagem

--- src/main/java/com/jdsjara/algafood/api/controller/CidadeControllerOpenApi.java
+++ src/main/java/com/jdsjara/algafood/api/controller/CidadeControllerOpenApi.java
@@ -5,6 +5,9 @@ import java.util.List;
 import com.jdsjara.algafood.api.model.CidadeModel;
 import com.jdsjara.algafood.api.model.input.CidadeInput;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
@@ -12,14 +15,26 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @Tag(name = "Cidades", description = "Gerencia as cidades")
 public interface CidadeControllerOpenApi {
 	
+	@Operation(summary = "Listar as cidades")
 	List<CidadeModel> listar();
 	
-	CidadeModel buscar(Long cidadeId);
+	@Operation(summary = "Buscar uma cidade por Id")
+	CidadeModel buscar(@Parameter(description = "Id de uma cidade", 
+		example = "1", required = true) Long cidadeId);
 
-	CidadeModel adicionar(CidadeInput cidadeInput);
+	@Operation(summary = "Cadastrar uma cidade", description = "Cadastro de uma cidade necessita "
+			+ "de um estado e um nome válido")
+	CidadeModel adicionar(@RequestBody(description = "Representação de uma nova cidade", 
+		required = true) CidadeInput cidadeInput);
 
-	CidadeModel atualizar(Long cidadeId, CidadeInput cidadeInput);
+	@Operation(summary = "Atualizar uma cidade")
+	CidadeModel atualizar(@Parameter(description = "Id de uma cidade", example = "1",
+		required = true) Long cidadeId,
+			@RequestBody(description = "Representação de uma nova cidade", 
+		required = true) CidadeInput cidadeInput);
 	
-	void remover(Long cidadeId);
+	@Operation(summary = "Excluir uma cidade")
+	void remover(@Parameter(description = "Id de uma cidade", example = "1",
+		required = true) Long cidadeId);
 	
 }
--- src/main/java/com/jdsjara/algafood/domain/service/CatalogoFotoProdutoService.java
+++ src/main/java/com/jdsjara/algafood/domain/service/CatalogoFotoProdutoService.java
@@ -1,5 +1,6 @@
 package com.jdsjara.algafood.domain.service;
 
+import java.io.InputStream;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.jdsjara.algafood.domain.model.FotoProduto;
 import com.jdsjara.algafood.domain.repository.ProdutoRepository;
+import com.jdsjara.algafood.domain.service.FotoStorageService.NovaFoto;
 
 @Service
 public class CatalogoFotoProdutoService {
@@ -15,8 +17,13 @@ public class CatalogoFotoProdutoService {
 	@Autowired
 	private ProdutoRepository produtoRepository;
 	
+	@Autowired
+	private FotoStorageService fotoStorageService;
+	
 	@Transactional
-	public FotoProduto salvar(FotoProduto foto) {
+	public FotoProduto salvar(FotoProduto foto, InputStream dadosArquivo) {
+		String nomeNovoArquivo = fotoStorageService.gerarNomeArquivo(foto.getNomeArquivo());
+		
 		// Se existir uma foto para o produto, o produto será excluído e
 		// será cadastrado uma nova foto para o produto. Se caso não existir um
 		// produto, será cadastrada a primeira foto para o produto.
@@ -26,7 +33,20 @@ public class CatalogoFotoProdutoService {
 			produtoRepository.delete(fotoExistente.get());
 		}
 		
-		return produtoRepository.save(foto);
+		foto.setNomeArquivo(nomeNovoArquivo);
+		foto =  produtoRepository.save(foto);
+		// flush() -> Força que o EntityManager do JPA descarregue no banco as 
+		// transações queestão na fila do JPA e devam ser realizadas
+		produtoRepository.flush();
+		
+		NovaFoto novaFoto = NovaFoto.builder()
+				.nomeArquivo(foto.getNomeArquivo())
+				.inputStream(dadosArquivo)
+				.build();
+				
+		fotoStorageService.armazenar(novaFoto);
+		
+		return foto;
 	}
 	
 }
